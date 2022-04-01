838f222f100e39e1bdbbedb86f0f78506bab1d62
ZavAlexandr <zavalex.srv@gmail.com>
2022-04-01 17:30:57 +0300

    Odoo Guidelines

--- my_academy/models/__init__.py
+++ my_academy/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from . import my_academy_course
-from . import my_academy_session
-from . import my_academy_session_lines
+from . import course
 from . import res_partner
+from . import session
+from . import session_lines

e597dcd30b257447bc70dbdbb7a34053af119686
ZavAlexandr <zavalex.srv@gmail.com>
2022-07-13 19:28:10 +0300

    Project time limit optimized

--- bs_project/models/models.py
+++ bs_project/models/models.py
@@ -6,15 +6,18 @@ from odoo import models, fields, api, _
 class bs_project(models.Model):
     _inherit = 'project.project'
 
-    work_hours = fields.Integer(_('Hours'))
-    work_minutes = fields.Integer(_('Minutes'))
+    bs_time_limit = fields.Float(_('Time limit (h:m)'))
+    bs_time_remain = fields.Float(_('Remain time (h:m)'), compute='_compute_remain_time')
 
-    @api.onchange('work_hours')
-    def _onchange_work_hours(self):
-        for rec in self:
-            rec.work_minutes = rec.work_hours * 60
+    def _compute_remain_time(self):
+        for doc in self:
+            if doc.bs_time_limit == 0:
+                doc.bs_time_remain = 0
+                return
 
-    @api.onchange('work_minutes')
-    def _onchange_work_minutes(self):
-        for rec in self:
-            rec.work_hours = round(rec.work_minutes / 60)
+            unit_amount = 0
+            data = self.env['account.analytic.line'].sudo().with_context().search([('project_id', '=', doc.id)])
+            for rec in data:
+                unit_amount += rec.unit_amount
+
+            doc.bs_time_remain = doc.bs_time_limit - unit_amount
